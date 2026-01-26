d1bff5033992d02ed62dc026ae37f81367dbdf75
Ludovic Coquemert <belethan@gmail.com>
2026-01-26 13:58:59 +0100

    Suite mise à jour

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,9 @@
  */
 import './styles/app.scss';
 import './styles/app.generated.css';
-import './styles/vendor/bootstrap/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
 console.log('AssetMapper + Sass OK');
 import './stimulus_bootstrap.js';
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
